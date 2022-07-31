a290c2daa062fd6416e798120b1cbbd59c720e53
krlvm <51774833+krlvm@users.noreply.github.com>
2022-08-01 03:15:49 +0400

    Support for shrinking File Explorer address bar height

--- ExplorerPatcher/dllmain.c
+++ ExplorerPatcher/dllmain.c
@@ -70,6 +70,7 @@ DWORD bOldTaskbar = TRUE;
 DWORD bWasOldTaskbarSet = FALSE;
 DWORD bAllocConsole = FALSE;
 DWORD bHideExplorerSearchBar = FALSE;
+DWORD bShrinkExplorerAdressBar = FALSE;
 DWORD bMicaEffectOnTitlebar = FALSE;
 DWORD bHideIconAndTitleInExplorer = FALSE;
 DWORD bHideControlCenterButton = FALSE;
@@ -5935,6 +5936,15 @@ void WINAPI LoadSettings(LPARAM lParam)
         dwSize = sizeof(DWORD);
         RegQueryValueExW(
             hKey,
+            TEXT("ShrinkExplorerAddressBar"),
+            0,
+            NULL,
+            &bShrinkExplorerAdressBar,
+            &dwSize
+        );
+        dwSize = sizeof(DWORD);
+        RegQueryValueExW(
+            hKey,
             TEXT("UseClassicDriveGrouping"),
             0,
             NULL,
@@ -9305,6 +9315,17 @@ int patched_GetSystemMetrics(int nIndex)
 #pragma endregion
 
 
+#pragma region "Shrink File Explorer address bar height on Windows 10 19H2+"
+int explorerframe_GetSystemMetricsForDpi(int nIndex, UINT dpi)
+{
+    if (nIndex == SM_CYFIXEDFRAME)
+	{
+		return 0;
+	}
+    return GetSystemMetricsForDpi(nIndex, dpi);
+}
+#pragma endregion
+
 DWORD InjectBasicFunctions(BOOL bIsExplorer, BOOL bInstall)
 {
     //Sleep(150);
@@ -9393,6 +9414,7 @@ DWORD InjectBasicFunctions(BOOL bIsExplorer, BOOL bInstall)
                 VnPatchIAT(hExplorerFrame, "user32.dll", "SetWindowLongPtrW", SetWindowLongPtrWHook);
             }
             VnPatchIAT(hExplorerFrame, "API-MS-WIN-CORE-STRING-L1-1-0.DLL", "CompareStringOrdinal", ExplorerFrame_CompareStringOrdinal);
+            VnPatchIAT(hExplorerFrame, "user32.dll", "GetSystemMetricsForDpi", explorerframe_GetSystemMetricsForDpi);
         }
         else
         {
